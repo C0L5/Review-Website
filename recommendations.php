35b6a9eff3fcfe32438dd624b74111d27929b9f7
Hendrix Mycole <62733144+C0L5@users.noreply.github.com>
2026-04-13 15:35:04 +1000

    Implemented API for upcoming games
    I used IGDB's API to fetch any upcoming games

--- recommendations.php
+++ recommendations.php
@@ -13,7 +13,9 @@
 <body class="d-flex flex-column">
     <!-- Navigation Bar -->
     <?php include 'include/navigationBar.php' ?>
-
+    <div class="container mt-4">
+        <div id="games" class="row"></div>
+    </div>
 
 
     <!--The footer-->
@@ -21,6 +23,37 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous">
     </script>
+    <script>
+        fetch('include/upcomingGamesAPI.php')
+            .then(res => res.json())
+            .then(games => {
+                const container = document.getElementById('games');
+
+                games.forEach(game => {
+                    const col = document.createElement('div');
+                    col.className = 'col-md-3 mb-4';
+
+                    const image = game.cover ?
+                        "https:" + game.cover.url.replace('t_thumb', 't_cover_big') : '';
+
+                    const date = game.first_release_date ?
+                        new Date(game.first_release_date * 1000).toLocaleDateString() : 'TBA';
+
+                    col.innerHTML = `
+        <div class="card h-100 shadow-sm">
+          ${image ? `<img src="${image}" class="card-img-top">` : ''}
+          <div class="card-body">
+            <h5 class="card-title">${game.name}</h5>
+            <p class="card-text"><strong>Release:</strong> ${date}</p>
+          </div>
+        </div>
+      `;
+
+                    container.appendChild(col);
+                });
+            });
+    </script>
+
 </body>
 
 </html>
